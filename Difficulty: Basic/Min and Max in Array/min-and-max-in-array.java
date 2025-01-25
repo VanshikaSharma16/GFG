968503a415d019bb21d31249614797dd3ca8860c
VanshikaSharma <137971960+VanshikaSharma16@users.noreply.github.com>
2025-01-25 23:44:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Min and Max in Array/min-and-max-in-array.java
@@ -0,0 +1,104 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+// User function Template for Java
+// User function Template for Java
+
+/*
+class Pair<K, V> {
+    private final K key;
+    private final V value;
+
+    public Pair(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+}
+
+Java users need to return result in Pair class
+For Example -> return new Pair(minimum,maximum)
+*/
+
+import java.util.*;
+
+class Solution {
+    public Pair<Integer, Integer> getMinMax(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            throw new IllegalArgumentException("Array cannot be null or empty");
+        }
+        
+        int mn = Integer.MAX_VALUE;
+        int mx = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+            if (num < mn) mn = num;
+            if (num > mx) mx = num;
+        }
+        
+        return new Pair<>(mn, mx); // Correct Pair instantiation
+    }
+}
+
+
+
+//{ Driver Code Starts.
+
+class Pair<K, V> {
+    private final K key;
+    private final V value;
+
+    public Pair(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey() { return key; }
+
+    public V getValue() { return value; }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+
+            Solution ob = new Solution();
+            Pair<Integer, Integer> pp = ob.getMinMax(arr);
+            System.out.println(pp.getKey() + " " + pp.getValue());
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
