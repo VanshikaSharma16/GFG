8bc7ad3ca2df6a10e2929733872fa0d4e3b8412c
VanshikaSharma <137971960+VanshikaSharma16@users.noreply.github.com>
2025-02-11 19:16:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/LCM And GCD/lcm-and-gcd.java
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+
+            int a;
+            a = Integer.parseInt(br.readLine());
+
+            int b;
+            b = Integer.parseInt(br.readLine());
+
+            Solution obj = new Solution();
+            int[] ans = obj.lcmAndGcd(a, b);
+            System.out.println(ans[0] + " " + ans[1]);
+            System.out.println("~");
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+// User function Template for Java
+
+class Solution {
+    private static int findGCD (int a, int b) {
+        while (b!= 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+    public static int[] lcmAndGcd(int a, int b) {
+        // code here
+        int gcd = findGCD(a, b);
+        int lcm = (a / gcd) * b;
+        return new int[]{lcm, gcd};
+    }
+}
+
+//{ Driver Code Starts.
+
+// } Driver Code Ends
