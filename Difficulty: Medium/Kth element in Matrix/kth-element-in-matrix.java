1418df586f59d276917fe0c0a3cea61312a0cb15
VanshikaSharma <137971960+VanshikaSharma16@users.noreply.github.com>
2025-07-06 23:10:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Kth element in Matrix/kth-element-in-matrix.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int kthSmallest(int[][] matrix, int k) {
+        // code here
+        ArrayList<Integer> al = new ArrayList<>();
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                al.add(matrix[i][j]);
+            }
+        }
+        Collections.sort(al);
+        return (al.get(k - 1));
+    }
+}
