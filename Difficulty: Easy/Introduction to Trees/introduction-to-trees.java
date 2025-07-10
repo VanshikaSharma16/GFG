6aec65ade1991127a27db1932f4ec3aea4a3b263
VanshikaSharma <137971960+VanshikaSharma16@users.noreply.github.com>
2025-07-10 23:30:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Introduction to Trees/introduction-to-trees.java
@@ -0,0 +1,9 @@
+// User function Template for Java
+import java.lang.Math.*;
+
+class Solution {
+    static int countNodes(int i) {
+        // code here
+        return (int) Math.pow(2, i-1);
+    }
+}
