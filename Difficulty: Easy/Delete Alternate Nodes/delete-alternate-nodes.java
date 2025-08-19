55caf3a72d4832878fc9ff8a417324c63f37e2d7
VanshikaSharma <137971960+VanshikaSharma16@users.noreply.github.com>
2025-08-19 17:33:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Delete Alternate Nodes/delete-alternate-nodes.java
@@ -0,0 +1,21 @@
+/*
+class Node {
+    int data;
+    Node next;
+
+    public Node(int data){
+        this.data = data;
+    }
+}
+*/
+class Solution {
+    public void deleteAlt(Node head) {
+        // Code Here
+        
+        Node temp = head;
+        while (temp != null && temp.next != null) {
+            temp.next = temp.next.next;
+            temp = temp.next;
+        }
+    }
+}
